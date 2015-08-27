1aeb5a67e66ba9903e5f41685ab88d9e30223545
Nick LoCascio <nlocascio@gmail.com>
2015-08-27 12:49:50 -0400

    Added Flashed Error handler

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -35,6 +35,15 @@
                 </div>
             </div>
 
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
             @yield('content')
         </div>
